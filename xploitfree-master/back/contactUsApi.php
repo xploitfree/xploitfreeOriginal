f877ee64d46b12443f0009397cdd0f2a278082a1
BIraj <birag.gpta@gmail.com>
2020-09-11 16:12:48 +0530

    ccccdddd

--- xploitfree-master/back/contactUsApi.php
+++ xploitfree-master/back/contactUsApi.php
@@ -14,22 +14,30 @@
         if ($name != "") {
             $name = filter_var($name, FILTER_SANITIZE_STRING);
             if ($name == "") {
-                $response['name'] = "Invalid Name";
-                
+                $response['message'] = "Invalid Name";
+                $response['success'] = false;
+                echo json_encode($response);
+                exit();
             }
         } else {
-            $response['name'] = "Invalid Name";
-               
+            $response['message'] = "Invalid Name";
+            $response['success'] = false;
+            echo json_encode($response);
+            exit();
         }
          //message
         if ($message != "") {
             $message = filter_var($message, FILTER_SANITIZE_STRING);
             if ($message == "") {
                 $response['message'] = "Invalid message";
-               
+                $response['success'] = false;
+                echo json_encode($response);
+                exit();
             }
         } else {
             $response['message'] = "Invalid message";
+            $response['success'] = false;
+            echo json_encode($response);
                 
         }
         //email
@@ -37,39 +45,54 @@
             $email = $string->email;
             $email = filter_var($email, FILTER_SANITIZE_EMAIL);
             if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-                $response['email'] = "$email is not a valid email address.";
+                $response['message'] = "email is not a valid email address.";
+                $response['success'] = false;
                 $email = "";
+                echo json_encode($response);
+                exit();
             }
         } else {
-            $response['email'] = "Not a valid email address.";
-            
+            $response['message'] = "Not a valid email address.";
+            $response['success'] = false;
+            echo json_encode($response);
+                exit();
         }
         //phone
         if($phone != ""){
             $phone = (int)$phone;
             $phone = filter_var($phone, FILTER_SANITIZE_NUMBER_INT);
             if (strlen($phone) > 10) {
-                $response['phone'] = "$phone is not valid.";
+                $response['message'] = "Phone is not valid.";
+                $response['success'] = false;
+                echo json_encode($response);
+                exit();
                 $phone = "";
             }else {
                 $phone = (string)$phone;
             }
         }else {
-            $response['phone'] = 'Please enter your Number.';
-            
+            $response['message'] = 'Please enter your Number.';
+            $response['success'] = false;
+            echo json_encode($response);
+            exit();
         }
         //subject
    if($sub != ""){
     $sub = filter_var($sub, FILTER_SANITIZE_STRING);
     if ($sub == "") {
-        $response['name'] = "Invalid subject";
+        $response['message'] = "Invalid subject";
+        $response['success'] = false;
+        echo json_encode($response);
+        exit();
      }
    }else {
-       $response['sub'] = "Subject cannot be null";
-       
+       $response['message'] = "Subject cannot be null";
+       $response['success'] = false;
+       echo json_encode($response);
+       exit();
    }
 
-   if ($name != "" && $phone != "" && $email != "" && $sub != ""){
+   if ($name != "" && $phone != "" && $email != "" && $sub != "" && $message !=""){
     $query1 = "INSERT INTO messagesTable1Num(phone,name,email) VALUES ('$phone','$name','$email')";
     $query2 = "INSERT INTO messagesTable2mes(sub,message,phone) VAlues ('$sub','$message','$phone')";
     $query_result1 = mysqli_query($con,$query1);
@@ -79,26 +102,20 @@
         $response['success'] = true;
         $response['message'] = "Your query has been submitted";  
         echo json_encode($response); 
+        exit();
     }
     else{
         
         $response['success'] = false;
         $response['message'] = "wrong credentials";
         echo json_encode($response);
+        exit();
     }
 
-    }else{
-        $response['general'] = "Wrong credentials";
-        echo json_encode($response);
     }
-
-    
-
 }else {
-    $response['general'] = "credentials cannot be null";
+    $response['message'] = "credentials cannot be null";
     echo json_encode($response);
 }
 
-    
-    
 ?>
